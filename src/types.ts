7a91bd340e56dc4206db5a6c8f69f7efc6e8f94b
Amnon Heiman <amnon@scylladb.com>
2020-07-29 15:06:13 +0300

    types: leave only the queryText field

--- src/types.ts
+++ src/types.ts
@@ -2,11 +2,10 @@ import { DataQuery, DataSourceJsonData } from '@grafana/data';
 
 export interface MyQuery extends DataQuery {
   queryText?: string;
-  constant: number;
 }
 
 export const defaultQuery: Partial<MyQuery> = {
-  constant: 6.5,
+  queryText: '',
 };
 
 /**
