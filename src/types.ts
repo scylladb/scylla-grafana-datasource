bd87eadd9c6bb21284be35a0e11736b8d997927d
Amnon Heiman <amnon@scylladb.com>
2023-08-25 12:58:39 -0400

    updated ts files

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ export interface MyQuery extends DataQuery {
   queryHost?: string;
 }
 
-export const defaultQuery: Partial<MyQuery> = {
+export const DEFAULT_QUERY: Partial<MyQuery> = {
   queryText: '',
   queryHost: '',
 };
